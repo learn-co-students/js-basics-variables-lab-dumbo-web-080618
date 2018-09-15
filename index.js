c140e585a5ae2aa97212e034584f8c79adeef50b
github username <github email address>
2018-09-15 12:12:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber'
+// mostProfitableNeighborhood
+// is declared as equal to Chelsea
+let mostProfitableNeighborhood = 'Chelsea'
+
+// companyCeo
+// is declared as equal to Susan Smith
+let companyCeo = "Susan Smith"
